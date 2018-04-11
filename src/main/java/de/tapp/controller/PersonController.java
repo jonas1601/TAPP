b1b9eccf7eb4253cecdb40c6d7cd0435dc22f4d2
Jonas Waldmann <Jonas-W97@gmx.de>
2018-04-11 09:15:04 +0200

    Controller-Änderungen Login und Termine pro Gruppe

--- src/main/java/de/tapp/controller/PersonController.java
+++ src/main/java/de/tapp/controller/PersonController.java
@@ -2,12 +2,16 @@ package de.tapp.controller;
 
 import de.tapp.application.HibernateConfiguration;
 import de.tapp.entity.Person;
+import de.tapp.entity.Termin;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PersonController {
@@ -21,7 +25,19 @@ public class PersonController {
         return person;
     }
 
-    @PostMapping(path = "/person")
+    @GetMapping(path = "/login")
+    public Person login(@RequestParam String benutzername,@RequestParam String pass){
+        Session session = HibernateConfiguration.getSessionFactory().openSession();
+       Person person = (Person)session.createCriteria(Person.class)
+                .add(Restrictions.eq("benutzername",benutzername))
+                .add(Restrictions.ge("pass",pass))
+                .uniqueResult();
+
+       return person;
+    }
+
+
+    @PostMapping(path = "/register")
     public HttpStatus addPerson(@RequestParam String benutzername, @RequestParam String vorname,
                                 @RequestParam String nachname, @RequestParam String pass,
                                 @RequestParam String notificationToken) {
--- src/main/java/de/tapp/controller/TerminController.java
+++ src/main/java/de/tapp/controller/TerminController.java
@@ -3,11 +3,15 @@ package de.tapp.controller;
 import de.tapp.application.HibernateConfiguration;
 import de.tapp.entity.Termin;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -20,6 +24,20 @@ public class TerminController {
         Termin termin = session.load(Termin.class, terminId);
         session.close();
         return termin;
+    }
+
+
+    @GetMapping(path = "/termine")
+    public List<Termin> getTermineByGruppenId(@RequestParam int gruppenId){
+        Session session = HibernateConfiguration.getSessionFactory().openSession();
+        LocalDateTime heutigerTag = LocalDateTime.now();
+        heutigerTag = heutigerTag.truncatedTo(ChronoUnit.DAYS);
+
+        List<Termin> termine = session.createCriteria(Termin.class)
+                .add(Restrictions.eq("gruppen_id",gruppenId))
+                .add(Restrictions.ge("anfang",heutigerTag))
+                .list();
+        return termine;
     }
 
     @PostMapping(path = "/termin")
