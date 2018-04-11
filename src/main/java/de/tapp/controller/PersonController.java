81c1ba507f62f3dc20472df3bb04f9c456871e14
Jonas Waldmann <Jonas-W97@gmx.de>
2018-04-11 19:01:33 +0200

    Login funktioniert

--- src/main/java/de/tapp/controller/PersonController.java
+++ src/main/java/de/tapp/controller/PersonController.java
@@ -1,19 +1,18 @@
 package de.tapp.controller;
 
 import de.tapp.application.HibernateConfiguration;
+import de.tapp.entity.Credentials;
 import de.tapp.entity.Person;
 import de.tapp.entity.Termin;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8100")
 public class PersonController {
 
     @GetMapping(path = "/person")
@@ -24,12 +23,12 @@ public class PersonController {
         return person;
     }
 
-    @GetMapping(path = "/login")
-    public Person login(@RequestParam String benutzername,@RequestParam String pass){
+    @PostMapping(path = "/login")
+    public Person login(@RequestBody Credentials credentials){
         Session session = HibernateConfiguration.getSessionFactory().openSession();
        Person person = (Person)session.createCriteria(Person.class)
-                .add(Restrictions.eq("benutzername",benutzername))
-                .add(Restrictions.ge("pass",pass))
+                .add(Restrictions.eq("benutzername",credentials.getUsername()))
+                .add(Restrictions.ge("pass",credentials.getPassword()))
                 .uniqueResult();
 
        return person;
@@ -37,10 +36,10 @@ public class PersonController {
 
 
     @PostMapping(path = "/register")
-    public HttpStatus addPerson(@RequestParam String benutzername, @RequestParam String vorname,
-                                @RequestParam String nachname, @RequestParam String pass,
-                                @RequestParam String notificationToken) {
-        Session session = null;
+        public HttpStatus addPerson(@RequestParam String benutzername, @RequestParam String vorname,
+                @RequestParam String nachname, @RequestParam String pass,
+                @RequestParam String notificationToken) {
+            Session session = null;
         try {
             Person person = new Person();
             person.setBenutzername(benutzername);
@@ -62,4 +61,7 @@ public class PersonController {
             }
         }
     }
+
+
+
 }
