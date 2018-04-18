de39dba6ce369ff711a23464c57eb7ea05f0e197
Jonas Waldmann <Jonas-W97@gmx.de>
2018-04-18 12:05:39 +0200

    Änderungen im PersonController für Register

--- src/main/java/de/tapp/controller/PersonController.java
+++ src/main/java/de/tapp/controller/PersonController.java
@@ -4,6 +4,7 @@ import de.tapp.application.HibernateConfiguration;
 import de.tapp.entity.Credentials;
 import de.tapp.entity.Person;
 import de.tapp.entity.Termin;
+import org.hibernate.ObjectNotFoundException;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.http.HttpStatus;
@@ -12,48 +13,68 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8100")
+@CrossOrigin
 public class PersonController {
 
     @GetMapping(path = "/person")
     public Person getPersonById(@RequestParam() int personId) {
+
         Session session = HibernateConfiguration.getSessionFactory().openSession();
-        Person person = session.load(Person.class, personId);
-        session.close();
+        Person person = null;
+        try{
+             person = session.load(Person.class, personId);
+
+        }catch(ObjectNotFoundException e){
+
+        }finally {
+            session.close();
+        }
+
         return person;
+
     }
 
     @PostMapping(path = "/login")
     public Person login(@RequestBody Credentials credentials){
         Session session = HibernateConfiguration.getSessionFactory().openSession();
-       Person person = (Person)session.createCriteria(Person.class)
-                .add(Restrictions.eq("benutzername",credentials.getUsername()))
-                .add(Restrictions.ge("pass",credentials.getPassword()))
-                .uniqueResult();
+        Person person = null;
+        try {
+           person = (Person) session.createCriteria(Person.class)
+                   .add(Restrictions.eq("benutzername", credentials.getUsername()))
+                   .add(Restrictions.ge("pass", credentials.getPassword()))
+                   .uniqueResult();
 
+       }catch (ObjectNotFoundException e){
+
+        }finally {
+            session.close();
+        }
        return person;
     }
 
 
     @PostMapping(path = "/register")
-        public HttpStatus addPerson(@RequestParam String benutzername, @RequestParam String vorname,
-                @RequestParam String nachname, @RequestParam String pass,
-                @RequestParam String notificationToken) {
+        public HttpStatus addPerson(@RequestBody Person person) {
             Session session = null;
-        try {
-            Person person = new Person();
+           /* Person person = new Person();
             person.setBenutzername(benutzername);
             person.setVorname(vorname);
             person.setNachname(nachname);
             person.setNotificationToken(notificationToken);
-            person.setPass(pass);
-            session = HibernateConfiguration.getSessionFactory().openSession();
-            session.beginTransaction();
-            session.save(person);
-            session.flush();
-            session.close();
-            return HttpStatus.ACCEPTED;
-        } catch (Exception e) {
+            person.setPass(pass);*/
+
+            try {
+                session = HibernateConfiguration.getSessionFactory().openSession();
+                session.beginTransaction();
+                person = (Person) session.createCriteria(Person.class)
+                        .add(Restrictions.eq("benutzername", person.getBenutzername()));
+                return HttpStatus.NOT_FOUND;
+            }catch (ObjectNotFoundException e){
+                session.save(person);
+                session.flush();
+                session.close();
+                return HttpStatus.ACCEPTED;
+            } catch (Exception e) {
             return HttpStatus.INTERNAL_SERVER_ERROR;
         } finally {
             if (session != null) {
